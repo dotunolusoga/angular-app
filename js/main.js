e7fc27c89c006889ba20a70426b75fd691282b7f
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-03-05 08:47:15 -0600

    created a family table, searchable and sortable, using AngularJS

--- js/main.js
+++ js/main.js
@@ -3,11 +3,102 @@ angular
 .controller('FamilyController', function($scope){
 	var family = this;
 
-	family.data=['Dotun', 'Deola', 'Tobi', 'Tosin', 'Tomi', 'Tope', 'Toke'];
 
-	family.removeFamily = function () {
-		var index = family.indexOf(name);
+    family.cohortOptions = [
+      'N/A',
+      'One',
+      'Two',
+      'Three',
+      'Four',
+      'Five',
+      'Six',
+      'Seven',
+      'Eight',
+      'Nine',
+      'Ten'
+    ];
+    
+	family.data = [
+	{
+		name: 'Dotun',
+		firstName: 'Adedotun',
+		middleName: 'Dee',
+		lastName: 'Olusoga',
+		current: true,
+		family: 1
+	},
+	{
+		name: 'Lamide',
+		firstName: 'Olamide',
+		middleName: 'Busayo',
+		lastName: 'Olusoga',
+		family: 1
+	},
+	{
+		name: 'Deola',
+		middleName: 'Adeola',
+		firstName: 'Mariam',
+		lastName: 'Yusuf',
+		family: 2
+	},
+	{
+		name: 'Tobi',
+		firstName: 'Tobiloba',
+		middleName: 'Fatimat',
+		lastName: 'Yusuf',
+		family: 2
+	},
+	{
+		name: 'Tosin',
+		firstName: 'Oluwatomisin',
+		middleName: 'Adedapo',
+		lastName: 'Ademola',
+		family: 3
+	},
+	{
+		name: 'Tomi',
+		firstName: 'Tomilola',
+		middleName: '',
+		lastName: 'Ademola',
+		family: 3
+	},
+	{
+		name: 'Tope',
+		firstName: 'Temitope',
+		middleName: '',
+		lastName: 'Ademola',
+		family: 3
+	},
+	{
+		name: 'Toke',
+		firstName: 'Tomiike',
+		middleName: '',
+		lastName: 'Ademola',
+		family: 3
+	}
+	];
+
+	family.newFam = {};
+
+	family.addFam = function () {
+		family.newFam.name;
+		family.newFam.firstName;
+		family.newFam.middleName;
+		family.newFam.lastName;
+		family.newFam.current;
+
+		family.data.push(family.newFam);
+		_clearForm();
+	}
+
+
+	family.removeFamily = function (person) {
+		var index = family.data.indexOf(person);
 		family.data.splice(index, 1)
+	}
+
+	function _clearForm (){
+		family.newFam = {};
 	}
 })
 
